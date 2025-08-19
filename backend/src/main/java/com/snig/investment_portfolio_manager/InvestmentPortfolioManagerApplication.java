55010835ca0ad83a6f1f145af293709d7d3d89e2
snig-17 <snigdha.17.tiwari@icloud.com>
2025-08-19 19:10:37 +0530

    user entity created

---
+++ backend/src/main/java/com/snig/investment_portfolio_manager/InvestmentPortfolioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.snig.investment_portfolio_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentPortfolioManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentPortfolioManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/snig/investment_portfolio_manager/InvestmentPortfolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snig.investment_portfolio_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentPortfolioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
